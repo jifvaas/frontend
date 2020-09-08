dae227b54cbf95fdd9ca8180f9620dd53d7f57c8
Kenzo Dominicus <kdominicus@gmail.com>
2020-09-08 23:03:17 +0200

    Remove empty constructor

--- src/service/EntryService.ts
+++ src/service/EntryService.ts
@@ -1,8 +1,8 @@
 export class EntryService {
     private entries = ['Kotlin', 'topografie', 'Sonos', 'Scala']; // dummy data, real data will come from jifvaas backend
-    constructor() {}
 
     async fetchEntries(): Promise<string[]> {
+        console.log('fetching')
         return Promise.resolve(this.entries);
     }
 }
