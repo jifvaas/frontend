d21f74c7f4dfdfe248af632bb6360391a8342bbf
Sander Koenders <sanderkoenders@gmail.com>
2020-09-10 10:23:37 +0200

    Fetch entries from the JIFVAAS API

--- src/service/EntryService.ts
+++ src/service/EntryService.ts
@@ -1,7 +1,7 @@
 export class EntryService {
-    private entries = ['Kotlin', 'topografie', 'Sonos', 'Scala']; // dummy data, real data will come from jifvaas backend
-
     async fetchEntries(): Promise<string[]> {
-        return Promise.resolve(this.entries);
+        return fetch('https://api.jimisfanvan.nl/')
+            .then(res => res.json())
+            .then(entries => entries.map((obj: any) => obj.value));
     }
 }
